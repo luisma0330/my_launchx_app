d798bddfae06af87cdd601a89fb87097bf74678f
Luis Ojeda <luisma0330@hotmail.com>
2022-04-29 18:14:49 -0500

    Realizando la prueba de unidad con el resultado pass

--- test/MissionCommander.test.js
+++ test/MissionCommander.test.js
@@ -2,6 +2,6 @@ describe("Unit Tests for Mission Commander Class", () => {
     test("1) Create a mission commander object", () => {
         const MissionCommander = require('./../app/missionCommander');
         const myMissionCommander = new MissionCommander("Woopa")
-        expect(myMissionCommander.name).toBe("Woops");
+        expect(myMissionCommander.name).toBe("Woopa");
     })
 })
