ab6ac1019c3a21a412d7ea449a397efaec118886
Luis Ojeda <luisma0330@hotmail.com>
2022-04-29 15:00:19 -0500

    Creando 3 objetos de la clase MissionCommander

---
+++ index.js
@@ -0,0 +1,9 @@
+const MissionCommander = require('./app/missionCommander');
+
+const luis = new MissionCommander("Luis")
+const manuel = new MissionCommander("Manuel")
+const vicky = new MissionCommander("Vicky")
+
+console.log(luis)
+console.log(manuel)
+console.log(vicky)
